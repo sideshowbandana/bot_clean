3da1f64a1ed766d650e959d1ca78d2ef244cc20c
Kyle Barton <kyle.humberto@gmail.com>
2016-08-01 14:53:09 -0700

    fix issue with input

--- lib/bot_clean/runner.rb
+++ lib/bot_clean/runner.rb
@@ -1,19 +1,14 @@
 class Runner
   attr_reader :bot
 
-  def get_input
-    pos = gets.split.map {|i| i.to_i}
+  def run
+    # is this a bug? I thought is was in the form "x y"
+    y, x = gets.split.map {|i| i.to_i}
     board = Array.new(5)
 
     (0...5).each do |i|
       board[i] = gets.strip
     end
-    @bot ||= Bot.new(Position.new(*pos), Board.from_array(board))
-  end
-
-  def run
-    while(get_input) do 
-      puts bot.next_move
-    end
+    puts Bot.new(Position.new(*pos), Board.from_array(board)).next_move
   end
 end
